5997e738cd942311a522f861e525a15a92b22bff
Justin Garza <jgarza9788@gmail.com>
2017-09-20 16:39:02 -0700

    final v1.0

--- index.js
+++ index.js
@@ -2,25 +2,47 @@ const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg')
 const {app, BrowserWindow, ipcMain} = electron;
 
-app.on('ready',()=>
-	{
-		console.log('app is ready');
+let mainWindow;
 
-		const mainWindow = new BrowserWindow({});
-		// mainWindow.loadURL('file://${__dirname}\index.html');
-		mainWindow.loadURL('file://' + __dirname + '/index.html');
-    
-	}
-);
+
+app.on('ready',()=>
+{
+	console.log('app is ready');
+
+	mainWindow = new BrowserWindow({});
+
+	/*code given by instructor that did not work*/
+	// mainWindow.loadURL('file://${__dirname}\index.html');
+	
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+});
 
 
 ipcMain.on('video:submit', (event, path)=>
 {
+
+	/*code given by instructor that did not work*/
+	/*
 	ffmpeg.ffprobe(path,(err,metadata)=>
 	{
-		console.log('duration is:', metadata.format.duration);
+		console.log('check check 1 2');
+		//console.log('duration is:', metadata.format.duration);
 	}
 	);
+	*/
+
+	//i have ffmpeg installed in two paths
+	//ffmpeg.setFfprobePath('C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin\\ffprobe.exe');
+	ffmpeg.setFfprobePath('C:\\ffmpeg\\bin\\ffprobe.exe');
+	ffmpeg.ffprobe(path,function(err, metadata) 
+	{
+		/*show all data*/
+		//console.log(require('util').inspect(metadata, false, null));
+		
+		console.log('duration is:', metadata.format.duration);
+		mainWindow.webContents.send('video:metadata',metadata.format.duration);
+	});
 }
 );
 
