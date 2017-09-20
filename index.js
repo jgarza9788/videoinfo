4ea17464a23054aba8cefe74296ea8a0e4a0e840
Justin Garza <jgarza9788@gmail.com>
2017-09-20 14:16:20 -0700

    video duration with ffprobe

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const ffmpeg = require('fluent-ffmpeg')
+const {app, BrowserWindow, ipcMain} = electron;
 
 app.on('ready',()=>
 	{
 		console.log('app is ready');
 
 		const mainWindow = new BrowserWindow({});
+		// mainWindow.loadURL('file://${__dirname}\index.html');
 		mainWindow.loadURL('file://' + __dirname + '/index.html');
-    // mainWindow.loadURL('file://${__dirname}\index.html');
-    // mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
-
-
-
+    
 	}
 );
+
+
+ipcMain.on('video:submit', (event, path)=>
+{
+	ffmpeg.ffprobe(path,(err,metadata)=>
+	{
+		console.log('duration is:', metadata.format.duration);
+	}
+	);
+}
+);
+
